6fe4a1b3cd340bf5291e5cfe363482b7e8948c13
Luis Santos <luis.correa@iteris.com.br>
2020-11-01 17:46:35 -0300

    Handling invalid input

--- src/main/java/br/com/luisergio/challenge/ChallengeApplication.java
+++ src/main/java/br/com/luisergio/challenge/ChallengeApplication.java
@@ -1,9 +1,20 @@
 package main.java.br.com.luisergio.challenge;
 
+import main.java.br.com.luisergio.challenge.business.impl.FactorialBusinessImpl;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * The type Challenge application.
+ */
 public class ChallengeApplication {
 
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
     public static void main(String [] args){
 
         Scanner sc = new Scanner(System.in);
@@ -26,27 +37,37 @@ public class ChallengeApplication {
             System.out.println();
 
             System.out.print("Execute: ");
-            option = sc.nextInt();
 
-            switch (option){
-                case 1:
-                    System.out.println("Executing 1...");
-                    break;
-                case 2:
-                    System.out.println("Executing 2...");
-                    break;
-                case 3:
-                    System.out.println("Executing 3...");
-                    break;
-                case 4:
-                    System.out.println("Executing 4...");
-                    break;
-                case -1:
-                    System.out.println("Exiting...");
-                    break;
-                default:
-                    System.out.println("ERROR: INVALID OPTION!");
-                    break;
+            try {
+                option = sc.nextInt();
+
+                switch (option){
+                    case 1:
+                        System.out.println("Executing 1...");
+                        break;
+                    case 2:
+                        System.out.println("Executing 2...");
+                        break;
+                    case 3:
+                        System.out.println("Executing 3...");
+                        FactorialBusinessImpl exe3 = new FactorialBusinessImpl();
+                        exe3.execute();
+                        break;
+                    case 4:
+                        System.out.println("Executing 4...");
+                        break;
+                    case -1:
+                        System.out.println("Exiting...");
+                        break;
+                    default:
+                        System.out.println("ERROR: INVALID OPTION!");
+                        break;
+                }
+
+            } catch (InputMismatchException ex){
+                System.out.println("ERROR: INVALID INPUT!");
+                option = 0;
+                sc.nextLine();
             }
 
             System.out.println();
