62c6449c0e9f40811761a53e493da039a404fa04
Luis Santos <luis.correa@iteris.com.br>
2020-11-01 17:46:46 -0300

    Factorial

---
+++ src/main/java/br/com/luisergio/challenge/business/ExerciseBusiness.java
@@ -0,0 +1,13 @@
+package main.java.br.com.luisergio.challenge.business;
+
+/**
+ * The interface Exercise business.
+ */
+public interface ExerciseBusiness {
+
+    /**
+     * Execute method to run the exercise.
+     */
+    void execute();
+
+}
---
+++ src/main/java/br/com/luisergio/challenge/business/impl/FactorialBusinessImpl.java
@@ -0,0 +1,47 @@
+package main.java.br.com.luisergio.challenge.business.impl;
+
+import main.java.br.com.luisergio.challenge.business.ExerciseBusiness;
+
+import java.util.Scanner;
+
+/**
+ * The type Exercise business.
+ */
+public class FactorialBusinessImpl implements ExerciseBusiness {
+
+    /**
+     * Execute method to run the factorial exercise.
+     */
+    @Override
+    public void execute() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Type the number to get the factorial of: ");
+
+        int value = sc.nextInt();
+        long factorial = this.getFactorial(value);
+
+        System.out.println("The factorial is: " + factorial);
+
+    }
+
+    /**
+     * Gets factorial.
+     *
+     * @param value the value to use on calculation
+     * @return the factorial
+     */
+    public long getFactorial(int value) {
+        if(value <= 1)
+            return 1;
+
+        long result = value;
+
+        for(int x = value-1; x > 1; x--) {
+            result = result * x;
+        }
+
+        return result;
+    }
+}
